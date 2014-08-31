60e5f9f3249afc414758ae9f3c4ec78d088c3d9d
aletker <aletker@gmail.com>
2014-08-31 16:50:03 -0300

    refatoramento

--- chat/chat/src/main/java/br/com/chat/Rede.java
+++ chat/chat/src/main/java/br/com/chat/Rede.java
@@ -1,5 +1,11 @@
 package br.com.chat;
 
+import java.rmi.RemoteException;
+
 public interface Rede {
+
+	void enviar(Pacote paccote) throws RemoteException;
+
+	boolean abrirConexao(String ip);
 
 }
